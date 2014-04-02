be638c03d896142a09eae5b11f7772db9e829dcd
Aya <aet.prass@khk.ee>
2014-04-02 20:38:40 +0300

    Elaborating views

--- views/tags/tags_view.php
+++ views/tags/tags_view.php
@@ -11,8 +11,8 @@
             <h1><a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post["post_subject"]?></a></h1>
             <p><?=$post["post_text"]?></p>
             <div>
-                <span class="badge badge-success">Posted <?=$post["post_created"]?></span>
-                <span class="badge badge-success">By <?=$post["username"]?></span>
+                <span class="badge badge-success">Kirja pandud <?=$post["post_created"]?></span>
+                <span class="badge badge-success">Kirja pannud <?=$post["username"]?></span>
                 <div class="pull-right">
                     <?if(!empty($tags)) foreach ($tags [$post ['post_id']] as $tag):?>
                     <a href="<?=BASE_URL?>tags/view/<?=$tag?>"><span class = "label" style="background-color: #5bc0de"><?=$tag?></span></a><?endforeach?>
